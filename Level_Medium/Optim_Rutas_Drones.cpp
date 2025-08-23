4147e85af52463ad5681916c7e50c6dd347a7a31
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-08-23 16:17:52 -0500

    ejercicio

--- Level_Medium/Optim_Rutas_Drones.cpp
+++ Level_Medium/Optim_Rutas_Drones.cpp
@@ -1,68 +1,60 @@
 #include <iostream>
 #include <vector>
-#include <map>
-#include <set>
+#include <cmath>
+#include <iomanip>
+#include <algorithm>
+#include <climits>
 using namespace std;
 
+double distancia(pair<int, int> a, pair<int, int> b) {
+    return sqrt(pow(a.first - b.first, 2) + pow(a.second - b.second, 2));
+}
+
 int main() {
-    int n, m;
-    cin >> n >> m;
-    vector<vector<int>> graph(n);
-    for (int i = 0; i < m; i++) {
-        int u, v;
-        cin >> u >> v;
-        graph[u].push_back(v);
-        graph[v].push_back(u);
-    }
-    
-    string estado_str;
-    cin >> estado_str;
-    vector<int> estado(n);
+    int n, k;
+    cin >> n >> k;
+    vector<pair<int, int>> puntos(n);
     for (int i = 0; i < n; i++) {
-        estado[i] = estado_str[i] - '0';
+        cin >> puntos[i].first >> puntos[i].second;
     }
     
-    int k;
-    cin >> k;
-    set<int> magnetizados;
-    for (int i = 0; i < k; i++) {
-        int node;
-        cin >> node;
-        magnetizados.insert(node);
+    vector<vector<double>> dist_entre(n, vector<double>(n, 0.0));
+    vector<double> dist_origen(n);
+    for (int i = 0; i < n; i++) {
+        dist_origen[i] = distancia({0,0}, puntos[i]);
+        for (int j = 0; j < n; j++) {
+            dist_entre[i][j] = distancia(puntos[i], puntos[j]);
+        }
     }
     
-    map<vector<int>, int> visited;
-    int steps = 0;
+    // DP: dp[mask][i] = distancia mínima para visitar los puntos en mask terminando en i
+    vector<vector<double>> dp(1 << n, vector<double>(n, INT_MAX));
+    for (int i = 0; i < n; i++) {
+        dp[1 << i][i] = 2 * dist_origen[i];
+    }
     
-    while (visited.find(estado) == visited.end()) {
-        visited[estado] = steps;
-        vector<int> next_estado = estado;
-        bool changed = false;
-        
-        for (int nodo = 0; nodo < n; nodo++) {
-            if (magnetizados.find(nodo) != magnetizados.end()) continue;
-            
-            int count_1 = 0;
-            for (int vecino : graph[nodo]) {
-                if (estado[vecino] == 1) count_1++;
-            }
-            
-            if (count_1 >= 2) {
-                next_estado[nodo] = 1 - estado[nodo];
-                changed = true;
+    for (int mask = 1; mask < (1 << n); mask++) {
+        for (int i = 0; i < n; i++) {
+            if (!(mask & (1 << i))) continue;
+            for (int j = 0; j < n; j++) {
+                if (mask & (1 << j)) continue;
+                int new_mask = mask | (1 << j);
+                double nuevo_valor = dp[mask][i] - dist_origen[i] + dist_entre[i][j] + dist_origen[j];
+                if (nuevo_valor < dp[new_mask][j]) {
+                    dp[new_mask][j] = nuevo_valor;
+                }
             }
         }
-        
-        if (!changed) {
-            cout << steps << endl;
-            return 0;
-        }
-        
-        estado = next_estado;
-        steps++;
     }
     
-    cout << -1 << endl;
+    double min_dist = INT_MAX;
+    for (int mask = 0; mask < (1 << n); mask++) {
+        if (__builtin_popcount(mask) != k) continue;
+        for (int i = 0; i < n; i++) {
+            if (dp[mask][i] < min_dist) min_dist = dp[mask][i];
+        }
+    }
+    
+    cout << fixed << setprecision(2) << min_dist << endl;
     return 0;
 }
-/*Verificación:*/
